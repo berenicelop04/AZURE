61023d14c7c560e5600189c734939d70a543c1a1
berenicelop04 <8122110004@utchetumal.edu.mx>
2025-07-05 21:04:07 -0500

    style/Se cambio el color y los iconos del sidebar.

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -1,56 +1,66 @@
-<aside class="sidebar-left border-right bg-white shadow" id="leftSidebar" data-simplebar>
-  <a href="#" class="btn collapseSidebar toggle-btn d-lg-none text-muted ml-2 mt-3" data-toggle="toggle">
+<aside class="sidebar-left border-right shadow" id="leftSidebar" data-simplebar style="background-color: #2A5C8A;">
+  <a href="#" class="btn collapseSidebar toggle-btn d-lg-none text-white ml-2 mt-3" data-toggle="toggle">
     <i class="fe fe-x"><span class="sr-only"></span></i>
   </a>
-  <nav class="vertnav navbar navbar-light">
+  <nav class="vertnav navbar navbar-dark">
     <div class="w-100 mb-4 d-flex">
-      <a class="navbar-brand mx-auto mt-2 flex-fill text-center" href="./index.html">
-        <svg version="1.1" id="logo" class="navbar-brand-img brand-sm" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 120 120">
-          <g>
-            <polygon class="st0" points="78,105 15,105 24,87 87,87" />
-            <polygon class="st0" points="96,69 33,69 42,51 105,51" />
-            <polygon class="st0" points="78,33 15,33 24,15 87,15" />
-          </g>
-        </svg>
+      <a class="navbar-brand mx-auto mt-2 flex-fill text-center" href="{{ route('dashboard') }}">
+        <img 
+          src="{{ asset('img/login/logo_smedi.jpg') }}" 
+          alt="SMEDI Logo" 
+          class="navbar-brand-img brand-sm" 
+          style="height: 60px; width: auto; max-width: 100%; object-fit: contain; padding: 4px;"
+        >
       </a>
     </div>
     <ul class="navbar-nav flex-fill w-100 mb-2">
         <li class="nav-item">
-            <a class="nav-link pl-3" href="{{ route('dashboard') }}">
+            <a class="nav-link pl-3 text-white" href="{{ route('dashboard') }}" style="background-color: rgba(255,255,255,0.15);">
             <i class="fe fe-home fe-16"></i>
             <span class="ml-1 item-text">Dashboard</span>
             </a>
         </li>
         <li class="nav-item">
-            <a class="nav-link pl-3" href="{{ route('municipios.index') }}">
-            <i class="fe fe-map fe-16"></i>
+            <a class="nav-link pl-3 text-white" href="{{ route('municipios.index') }}">
+            <i class="fe fe-map-pin fe-16"></i>
             <span class="ml-1 item-text">Municipios</span>
             </a>
         </li>
         <li class="nav-item">
-            <a class="nav-link pl-3" href="{{ route('localidades.index') }}">
-            <i class="fe fe-map fe-16"></i>
+            <a class="nav-link pl-3 text-white" href="{{ route('localidades.index') }}">
+            <i class="fe fe-navigation fe-16"></i>
             <span class="ml-1 item-text">Localidades</span>
             </a>
         </li>
         <li class="nav-item">
-            <a class="nav-link pl-3" href="{{ route('estado-energia.index') }}">
-            <i class="fe fe-map fe-16"></i>
+            <a class="nav-link pl-3 text-white" href="{{ route('estado-energia.index') }}">
+            <i class="fe fe-battery fe-16"></i>
             <span class="ml-1 item-text">Estado de Energia</span>
             </a>
         </li>
         <li class="nav-item">
-            <a class="nav-link pl-3" href="{{ route('dispositivos.index') }}">
-            <i class="fe fe-map fe-16"></i>
+            <a class="nav-link pl-3 text-white" href="{{ route('dispositivos.index') }}">
+            <i class="fe fe-hard-drive fe-16"></i>
             <span class="ml-1 item-text">Dispositivos</span>
             </a>
         </li>
         <li class="nav-item">
-            <a class="nav-link pl-3" href="{{ route('ubicacion_antenas.index') }}">
-            <i class="fe fe-map fe-16"></i>
+            <a class="nav-link pl-3 text-white" href="{{ route('ubicacion_antenas.index') }}">
+            <i class="fe fe-wifi fe-16"></i>
             <span class="ml-1 item-text">Ubicacion Antenas</span>
             </a>
         </li>
     </ul>
   </nav>
 </aside>
+
+<style>
+    /* Estilos personalizados */
+    #leftSidebar {
+        --sidebar-blue: #2A5C8A;
+        --hover-blue: #1E4A6D;
+        --active-blue: #3A7BAA;
+    }
+    
+  
+</style>
